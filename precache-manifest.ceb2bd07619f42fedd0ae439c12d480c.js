94cd80c2cd0a1d61f4522d900819402b230fa205
JohnVemula1998 <johnvemula7771998@gmail.com>
2022-02-11 21:41:39 +0530

    Updates

---
+++ precache-manifest.ceb2bd07619f42fedd0ae439c12d480c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a970a7b0a79f605ad28",
+    "url": "/react-blog/static/css/main.1a87bfa0.chunk.css"
+  },
+  {
+    "revision": "6a970a7b0a79f605ad28",
+    "url": "/react-blog/static/js/main.3667c1a4.chunk.js"
+  },
+  {
+    "revision": "392c4dcf4c910d970791",
+    "url": "/react-blog/static/js/runtime~main.a8b04b7f.js"
+  },
+  {
+    "revision": "bb13a5f8b9bb86804680",
+    "url": "/react-blog/static/js/2.3be7df77.chunk.js"
+  },
+  {
+    "revision": "8ebb8ad53c12249c7d32306a06082198",
+    "url": "/react-blog/index.html"
+  }
+];
